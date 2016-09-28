8b4c2636eba63f72c65ed9d2b22be0bbb6a6c4ee
Matt Rice <mattrice12@outlook.com>
2016-09-28 18:17:28 -0500

    can now render file outside of views; still working on getting React to work

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   def index
     deck = Deck.first
     cards = deck.cards.shuffle
-    render locals: { deck: deck, cards: cards }
+    render file: '../dist/index.html', locals: { deck: deck, cards: cards }
   end
 end
---
+++ app/entry.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import router from './assets/javascripts/components/router';
+
+ReactDOM.render(router, document.getElementById('container'));
