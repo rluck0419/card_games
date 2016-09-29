c15d0f051960d04db48f32762ade8f5b2332c714
Matt Rice <mattrice12@outlook.com>
2016-09-29 14:28:55 -0500

    got React to access objects in the back-end

--- FrontEnd/scripts/Components/LandingPage.js
+++ FrontEnd/scripts/Components/LandingPage.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import $ from 'jquery';
 
 export default React.createClass({
+  getInitialState() {
+    return {
+      showP: false,
+      nothing: true,
+    }
+  },
+  toggleP() {
+    this.setState({showP: !this.state.showP});
+  },
+  showValue() {
+    // console.log(this.refs.butts.value);
+  },
+  componentDidMount() {
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:3000/dashboard',
+      success: (response) => {
+        console.log(response);
+      }
+    })
+  },
   render() {
+    let p;
+    if (this.state.showP) {
+      p = (<input type="text" ref="butts" onClick={this.showValue}/>);
+    }
     return (
-      <div>sup dog</div>
+      <div className="first-div" onClick={this.toggleP}>
+        {p}
+      </div>
     );
   }
 });
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   def index
     deck = Deck.first
     cards = deck.cards.shuffle
-    render file: '../../FrontEnd/index.html', locals: { deck: deck, cards: cards }
+    # render file: '../../FrontEnd/index.html', locals: { deck: deck, cards: cards }
+    render json: {deck: deck.to_json, cards: cards.to_json}, status: 200
   end
 end
